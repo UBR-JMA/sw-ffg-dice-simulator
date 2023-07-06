4daaa2f595e29ca477ba99bc6f2d742656ed3665
Joshua Allison <78192304+josh-allison@users.noreply.github.com>
2023-07-06 15:53:53 -0700

    init

---
+++ src/DieFace.js
@@ -0,0 +1,23 @@
+
+
+export default class DieFace {
+    constructor(face) {
+        this.face = face;
+        this.possibleOutcomes = {
+            "BLANK":                {'s': 0, 'a': 0, 'f': 0, 't': 0, 'T': 0, 'D': 0},
+            "SUCCESS":              {'s': 1, 'a': 0, 'f': 0, 't': 0, 'T': 0, 'D': 0},
+            "ADVANTAGE":            {'s': 0, 'a': 1, 'f': 0, 't': 0, 'T': 0, 'D': 0},
+            "SUCCESS ADVANTAGE":    {'s': 1, 'a': 1, 'f': 0, 't': 0, 'T': 0, 'D': 0},
+            "DOUBLE SUCCESS":       {'s': 2, 'a': 0, 'f': 0, 't': 0, 'T': 0, 'D': 0},
+            "DOUBLE ADVANTAGE":     {'s': 0, 'a': 2, 'f': 0, 't': 0, 'T': 0, 'D': 0},
+            "TRIUMPH":              {'s': 0, 'a': 0, 'f': 0, 't': 0, 'T': 1, 'D': 0},
+            "FAILURE":              {'s': 0, 'a': 0, 'f': 1, 't': 0, 'T': 0, 'D': 0},
+            "THREAT":               {'s': 0, 'a': 0, 'f': 0, 't': 1, 'T': 0, 'D': 0},
+            "FAILURE_THREAT":       {'s': 0, 'a': 0, 'f': 1, 't': 1, 'T': 0, 'D': 0},
+            "DOUBLE FAILURE":       {'s': 0, 'a': 0, 'f': 2, 't': 0, 'T': 0, 'D': 0},
+            "DOUBLE THREAT":        {'s': 0, 'a': 0, 'f': 0, 't': 2, 'T': 0, 'D': 0},
+            "DESPAIR":              {'s': 0, 'a': 0, 'f': 0, 't': 0, 'T': 0, 'D': 1},
+        }
+        this.value = this.possibleOutcomes[face.toUpperCase()];
+    }
+}
